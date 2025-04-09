75facda0e4eec98e111ee5644354fda433b3209c
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-04-09 15:56:29 -0300

    feat: create and impl PersonModel

---
+++ Models/PersonModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Crud_Person.Models;
+
+public class PersonModel
+{
+    public PersonModel(string name)
+    {
+        Name = name;
+        Id = Guid.NewGuid();
+    }
+    
+    public Guid Id { get; init; }
+    public string Name { get; private set; }
+}
