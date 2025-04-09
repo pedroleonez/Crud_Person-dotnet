70a85a858c0440f897a0b2be988ac738d4b95e9b
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-04-09 15:57:18 -0300

    feat: create and impl PersonRoute

---
+++ Routes/PersonRoute.cs
@@ -0,0 +1,9 @@
+﻿namespace Crud_Person.Routes;
+
+public static class PersonRoute
+{
+    public static void PersonRoutes(this WebApplication app)
+    {
+        app.MapGet("person", () => "Hello you!");
+    }
+}
